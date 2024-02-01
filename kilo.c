d8d6c4d377ef4866abe46e47f0e66c76ac5373b1
Saswat Samal <saswat1091@outlook.com>
2024-02-02 00:19:08 +0530

    turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,4 +1,15 @@
 #include<unistd.h>
+#include<termios.h>
+
+void enableRawMode(){
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(){
   char c;
